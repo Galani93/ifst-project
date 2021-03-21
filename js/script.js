412593aa2ad0f52f4045faca259a2e8d1761b933
aniga <ani_galstyan_93@mail.ru>
2021-03-21 16:19:11 +0300

    Slider adaptive mobile version

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+$(function(){
+    $('.header .menu-button').click(function() {
+        $(this).toggleClass('active');
+        $('.header .header-contact').toggleClass('opened');
+    });
+    $('.totop-link a, .header ul li a').bind("click", function(e){
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top
+        }, 1000);
+        e.preventDefault();
+    });
+
+});
+
+
 $('.carousel[data-type="multi"] .item').each(function() {
     var next = $(this).next();
     if (!next.length) {
